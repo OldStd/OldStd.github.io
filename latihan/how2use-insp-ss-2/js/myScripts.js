e75b8b0c1342138f06d0752fbe1ddeb8f2e4f3cb
OldStd <OldStd@users.noreply.github.com>
2016-10-17 00:03:22 +0800

    Add files via upload

---
+++ latihan/how2use-insp-ss-2/js/myScripts.js
@@ -0,0 +1,10 @@
+/* js codes here */
+function myFunction (num){
+var ifrm_notes = document.getElementById("iframe_notes");
+var nts = "Step" + num + ".html";
+ifrm_notes.src = nts;
+
+var img_display1 = document.getElementById("img_display");
+var wdsp = "images\\Step_" + num + ".png";
+img_display1.src = wdsp;
+}
